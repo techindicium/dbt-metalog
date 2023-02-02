c178c3bf556702b474bf1758d914e9cf350f2ec5
Bruno Souza de Lima <bruno.szdl@gmail.com>
2023-02-02 11:45:08 -0300

    added description macro

---
+++ integration_tests/models/description_tests/description_view_path.sql
@@ -0,0 +1,15 @@
+{{ metalog.create_description_model(
+        resource_type = [
+                "model"
+                , "seed"
+        ]
+        , show_resource_type = True
+        , resource_path = [
+            "models/"
+            , "seeds/"
+        ]
+        , exclude_resource_path = [
+            'models/metadata_tests'
+            'models/description_tests'
+        ]
+)}}
